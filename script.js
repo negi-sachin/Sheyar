c607a37bc7a048fff2dd8f5218c234a6c22a0d93
Sachin negi <san.negi.77777@gmail.com>
2020-06-21 11:09:58 +0530

    download commented

--- script.js
+++ script.js
@@ -167,7 +167,7 @@ function execute() {
 
       console.log("Received", file);
 
-       download(file, fileInfo.name);
+      // download(file, fileInfo.name);
       filesDownloaded++;
       totalDownloaded = 0;
       fileChunks = [];
